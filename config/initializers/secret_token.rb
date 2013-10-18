43144a71795c6c446f8a67d76fc0b53c6e35a2bd
BryanCunningham <bryan.cunningham@me.com>
2013-10-17 21:01:28 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JuliesList::Application.config.secret_key_base = '9794cd68d30436078ec2cfa88929fe08f723b76729f21ddd66b1e010fb9530e3d3c378064e27ae4397a847bfb01d1817378f5e9c01bd712ac24d482a7726f9ad'
