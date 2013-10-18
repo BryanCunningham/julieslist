ca70497d4b281e6f81f1b734d39a0fd5f53de363
BryanCunningham <bryan.cunningham@me.com>
2013-10-18 17:07:37 -0400

    made basic user model

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'slim'
+gem 'bcrypt-ruby'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.0'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+    validates :name, presence: true
+    validates :email, presence: true format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }\
+    has_secure_password
+    validates :password, length { minimum: 6 }
 end
