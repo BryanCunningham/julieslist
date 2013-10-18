7939b64962047ac9f92a0bdfd070828b2016f582
BryanCunningham <bryan.cunningham@me.com>
2013-10-18 14:13:56 -0400

    made user scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131017174937) do
+ActiveRecord::Schema.define(version: 20131018175139) do
 
   create_table "ideas", force: true do |t|
     t.string   "title"
     t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
